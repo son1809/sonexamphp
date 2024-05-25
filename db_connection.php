aa4b836434bc79b19a1baf227e21a18b14ffeb8d
Xuan Son <maixuanson18092005@gmail.com>
2024-05-25 09:41:51 +0700

    exam

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,11 @@
-<!-- db_connection.php -->
 <?php
 $servername = "localhost";
-$username = "examphp"; // Your MySQL username
-$password = "password"; // Your MySQL password
+$username = "examphp";
+$password = "password";
 $dbname = "phonebook";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
